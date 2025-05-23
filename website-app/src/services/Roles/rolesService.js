965ecde7a3785816e3762f40b5b67c8055acb04c
Đỗ Kim Nhường <127850790+KimNhuong@users.noreply.github.com>
2025-05-23 14:01:08 +0700

    Theme modification

--- website-app/src/services/Roles/rolesService.js
+++ website-app/src/services/Roles/rolesService.js
@@ -1,8 +1,10 @@
-import { notify } from '../../components/Notification/notification';
+import { notify } from "../../components/Notification/notification";
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const getRoles = async (userId) => {
   try {
-    const response = await fetch(`http://localhost:5000/roles/show?userId=${userId}`, {
+    const response = await fetch(`${API_URL}/roles/show?userId=${userId}`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -17,27 +19,31 @@ export const getRoles = async (userId) => {
     console.error("Lỗi khi gọi API:", error);
     throw error;
   } finally {
-
   }
 };
 
-export const createRole = async (newRoleData,user) => {
+export const createRole = async (newRoleData, user) => {
   try {
-    const response = await fetch("http://localhost:5000/roles/create", {
+    const response = await fetch(`${API_URL}/roles/create`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({newRoleData, user}),
+      body: JSON.stringify({ newRoleData, user }),
     });
     const data = await response.json();
-    if(data.message === "Không có quyền truy cập" || data.message === "Vai trò không tồn tại" || data === "Role không được cung cấp" || data.message === "Vai trò đã tồn tại"){
-      notify(2,data.message,"Thất bại");
-    }else{
-      notify(1,"Tạo thành công quyền mới","Thành công");
+    if (
+      data.message === "Không có quyền truy cập" ||
+      data.message === "Vai trò không tồn tại" ||
+      data === "Role không được cung cấp" ||
+      data.message === "Vai trò đã tồn tại"
+    ) {
+      notify(2, data.message, "Thất bại");
+    } else {
+      notify(1, "Tạo thành công quyền mới", "Thành công");
     }
   } catch (error) {
-    notify(2,"Tạo quyền mới thất bại","Thất bại");
+    notify(2, "Tạo quyền mới thất bại", "Thất bại");
     console.error("Error creating role:", error);
     throw error;
   }
@@ -45,7 +51,7 @@ export const createRole = async (newRoleData,user) => {
 
 export const deleteRole = async (roleId, user) => {
   try {
-    const response = await fetch(`http://localhost:5000/roles/delete/${roleId}`, {
+    const response = await fetch(`${API_URL}/roles/delete/${roleId}`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
@@ -57,14 +63,18 @@ export const deleteRole = async (roleId, user) => {
     });
     const data = await response.json();
     console.log(data);
-    if(data.message == "Không có quyền truy cập" || data.message === "Vai trò không tồn tại" || data === "Role không được cung cấp"){
-      notify(2,data.message,"Thất bại");
-    }else{
-      notify(1,"Xóa quyền thành công","Thành công");
+    if (
+      data.message == "Không có quyền truy cập" ||
+      data.message === "Vai trò không tồn tại" ||
+      data === "Role không được cung cấp"
+    ) {
+      notify(2, data.message, "Thất bại");
+    } else {
+      notify(1, "Xóa quyền thành công", "Thành công");
     }
     return data;
   } catch (error) {
-    notify(2,"Xóa quyền thất bại","Thất bại");
+    notify(2, "Xóa quyền thất bại", "Thất bại");
     console.error("Error deleting role:", error);
     throw error;
   }
